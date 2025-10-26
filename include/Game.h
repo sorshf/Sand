d6983b7d43aa65869ca9c03095118fe0c7eb66ac
Soroush Shahryari Fard <soroush.shahryarifard@gmail.com>
2025-10-26 11:51:11 -0400

    Created inputHandler class.

--- include/Game.h
+++ include/Game.h
@@ -5,6 +5,7 @@
 #include <iostream>
 #include "Config.h"
 #include "World.h"
+#include "InputHandler.h"
 
 using namespace std;
 
@@ -14,19 +15,16 @@ private:
     int m_width;
     int m_height;
     World m_world;
+    InputHandler m_inputHandler;
     SDL_Window* m_window = nullptr;
     SDL_Renderer* m_renderer = nullptr;
     SDL_Color m_baseColor {194, 178, 128, 1}; //to be removed with better logic
     bool m_quit = false;
-    bool m_mousePressed = false;
-    SDL_Event m_e;
-    float m_x_click = 0.0f;
-    float m_y_click = 0.0f;
 public:
     Game(const char* title, int width, int height);
     ~Game();
     void processInput();
-    // void update();
+    void update();
     void render();
     void run();
 };
@@ -42,7 +40,7 @@ SDL_Color getRandomColor(){
 }
 
 Game::Game(const char* title, int width, int height):
-    m_title{title}, m_width(width), m_height(height), m_world{height, width} {
+    m_title{title}, m_width(width), m_height(height), m_world{height, width}, m_inputHandler{height, width}  {
 
     SDL_CreateWindowAndRenderer(m_title, m_width, m_height, 0, 
         &m_window, &m_renderer);
@@ -65,30 +63,18 @@ Game::~Game() {
 
 void Game::processInput() {
 
-    while (SDL_PollEvent(&m_e)) {            
-        switch (m_e.type) {
-            case SDL_EVENT_QUIT:
-                m_quit = true;
-                break;
-            case SDL_EVENT_MOUSE_BUTTON_UP:
-                m_mousePressed = false;
-                break;
-            case SDL_EVENT_MOUSE_BUTTON_DOWN:
-                m_mousePressed = true;
-                m_baseColor = getRandomColor();
-            case SDL_EVENT_MOUSE_MOTION:
-                if (m_mousePressed){
-                    SDL_GetMouseState(&m_x_click, &m_y_click);
-                    if (m_x_click >= 0 && m_y_click >= 0 && m_x_click<m_width && m_y_click<m_height)
-                        m_world.addSands(SDL_floorf(m_x_click), SDL_floorf(m_y_click), m_baseColor);
-                }
-                    break;
+    m_inputHandler.handleInput();
 
-            default:
-                break;
-        }
+    if (m_inputHandler.m_quit){
+        m_quit = true;
     }
 
+}
+
+void Game::update() {
+    if (m_inputHandler.m_addMaterial && m_inputHandler.m_addSands) {
+        m_world.addSands(SDL_floorf(m_inputHandler.m_x_click), SDL_floorf(m_inputHandler.m_y_click), m_baseColor);
+    }
 }
 
 void Game::render() {
@@ -112,7 +98,7 @@ void Game::render() {
 void Game::run() {
     while (!m_quit) {
         processInput();
-        // update();
+        update();
         render();
     }
 }
---
+++ include/InputHandler.h
@@ -0,0 +1,86 @@
+#ifndef INPUTHANDLER_H
+#define INPUTHANDLER_H
+
+#include <SDL3/SDL.h>
+#include <iostream>
+
+
+class InputHandler
+{
+private:
+    const int m_heightLimit;
+    const int m_widthLimit;
+    SDL_Event m_e;
+public:
+    bool m_quit = false;
+    bool m_mousePressed = false;
+    bool m_addMaterial = false;
+    bool m_addSands = true;
+    bool m_addWoods = false;
+    float m_x_click = 0.0f;
+    float m_y_click = 0.0f;
+    InputHandler(int heightLimit, int widthLimit);
+    ~InputHandler();
+    void handleInput();
+    bool mouseWithinBounds();
+};
+
+InputHandler::InputHandler(int heightLimit, int widthLimit):
+    m_heightLimit(heightLimit), m_widthLimit(widthLimit)
+{
+}
+
+InputHandler::~InputHandler()
+{
+}
+
+bool InputHandler::mouseWithinBounds(){
+    return (m_x_click >= 0 && m_y_click >= 0 && 
+        m_x_click<m_widthLimit && m_y_click<m_heightLimit);
+
+}
+
+void InputHandler::handleInput(){
+
+    while (SDL_PollEvent(&m_e)) {            
+        switch (m_e.type) {
+            case SDL_EVENT_QUIT:
+                m_quit = true;
+                break;
+            case SDL_EVENT_MOUSE_BUTTON_UP:
+                m_addMaterial = false;
+                m_mousePressed = false;
+                break;
+            case SDL_EVENT_MOUSE_BUTTON_DOWN:
+                m_mousePressed = true;
+                SDL_GetMouseState(&m_x_click, &m_y_click);
+                if (mouseWithinBounds()) {
+                    m_addMaterial = true;
+                }
+                break;
+            case SDL_EVENT_MOUSE_MOTION:
+                if (m_mousePressed){
+                    SDL_GetMouseState(&m_x_click, &m_y_click);
+                    if (mouseWithinBounds()) {
+                        m_addMaterial = true;
+                    }
+                }
+                    break;
+            case SDL_EVENT_KEY_DOWN:
+                if (m_e.key.key == SDLK_W) {
+                    m_addWoods = true;
+                    m_addSands = false;
+                } else if (m_e.key.key == SDLK_S) {
+                    m_addWoods = false;
+                    m_addSands = true;
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+}
+
+
+#endif
